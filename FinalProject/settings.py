26bd9a33c8ad8bedea32dbeb7356de5be34fae55
Paul <pbalafai@gmail.com>
2023-05-02 01:54:43 +0200

    secret key fixed

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
+from django.core.management.utils import get_random_secret_key
 
 
 
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'abracadabrawdsiuklasdf')
-
+SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
